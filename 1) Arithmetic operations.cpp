3431845eea05132e4af0268b8f838b6e507452dd
nhktyagi <84965999+nhktyagi@users.noreply.github.com>
2023-09-12 17:07:13 +0530

    Add files via upload

---
+++ 1) Arithmetic operations.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>>
+int main(){
+
+int a,b,c,d,e,f,g,h,i;
+printf("Enter the value of a ");
+scanf("%d",&a);
+printf("Enter the value of b ");
+scanf("%d",&b);
+c=a+b;
+d=a*b;
+e=a/b;
+f=a%b;
+
+printf("The two numbers are %d %d" ,a,b);
+printf(" \n The addition of the two numbers is %d" ,c);
+printf(" \n The multiplication of the two numbers is %d" ,d);
+printf(" \n The division of the two number will give %d ",e);
+printf(" \n The remainder is %d",f);
+}
